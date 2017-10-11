5be223723ba0c0bdcacf2b881857a583b123c862
Stacy-Q <963356295@qq.com>
2017-10-10 20:01:37 -0700

    Add files via upload

--- ants.py
+++ ants.py
@@ -136,7 +136,9 @@ class Bee(Insect):
         """Return True if this Bee cannot advance to the next Place."""
         # Phase 4: Special handling for NinjaAnt
         # BEGIN Problem 8
-        return self.place.ant is not None
+        return self.place.ant is not None and self.place.ant.blocks_path
+        # if self.place.ant is not None:
+        #   return self.place.ant.blocks_path
         # END Problem 8
 
     def action(self, colony):
@@ -157,6 +159,7 @@ class Ant(Insect):
     is_ant = True
     implemented = False  # Only implemented Ant classes should be instantiated
     food_cost = 0
+    blocks_path = True
 
     def __init__(self, armor=1):
         """Create an Ant with an ARMOR quantity."""
@@ -296,6 +299,12 @@ class ShortThrower(ThrowerAnt):
 
 
 # BEGIN Problem 7
+class WallAnt(Ant):
+  name = "Wall"
+  implemented = True
+  food_cost = 4
+  def __init__ (self, armor=4):
+    super().__init__(armor)
 # The WallAnt class
 # END Problem 7
 
@@ -306,12 +315,15 @@ class NinjaAnt(Ant):
     name = 'Ninja'
     damage = 1
     # BEGIN Problem 8
-    implemented = False   # Change to True to view in the GUI
+    implemented = True   # Change to True to view in the GUI
+    blocks_path = False
+    food_cost = 5
     # END Problem 8
 
     def action(self, colony):
         # BEGIN Problem 8
-        "*** YOUR CODE HERE ***"
+        for bee in list(self.place.bees):
+          bee.reduce_armor(self.damage)
         # END Problem 8
 
 
